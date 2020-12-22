e0371dc0fa8cbb713dd17d23861defc28f0a1eaa
chrsolr <chr.solr@gmail.com>
2020-12-22 16:35:02 -0600

    Added Bottom Tab Navigation Placeholder

--- App.tsx
+++ App.tsx
@@ -1,22 +1,89 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import React from "react";
+import "react-native-gesture-handler";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { colors } from "./utils/colors";
+
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Home!</Text>
+    </View>
+  );
+}
+
+function AccountScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Account!</Text>
+    </View>
+  );
+}
+
+function MoreScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>More!</Text>
+    </View>
+  );
+}
+
+function SettingsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Settings!</Text>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Apex Legends Buddy</Text>
-      <Text>Under Development</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Home') {
+              iconName = focused
+                ? 'ios-home'
+                : 'ios-home-outline';
+            } else if (route.name === 'More') {
+              iconName = focused ? 'ios-ellipsis-horizontal' : 'ios-ellipsis-horizontal-outline';
+            } else if (route.name === 'Settings') {
+              iconName = focused ? 'ios-settings' : 'ios-settings-outline';
+            } else if (route.name === 'Account') {
+              iconName = focused ? 'ios-person-circle' : 'ios-person-circle-outline';
+            }
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: colors.brand.accent,
+          inactiveTintColor: colors.gray,
+        }}
+      >
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="More" component={MoreScreen} />
+        <Tab.Screen name="Settings" component={SettingsScreen} />
+        <Tab.Screen name="Account" component={AccountScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ utils/colors.ts
@@ -0,0 +1,17 @@
+export const colors = {
+  background: {
+    main: '#fff',
+  },
+  text: {
+    primary: '#6f6f6f',
+    secondary: '#bdbdbd',
+    light: '#ecf0f1',
+  },
+  brand: {
+    accent: '#e64a19',
+  },
+  white: '#fff',
+  black: '#000',
+  light_gray: '#f1f1f1',
+  gray: '#A1A1A1',
+};
