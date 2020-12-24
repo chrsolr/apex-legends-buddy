ca22ca5f61c3725d3b7003c46c020643905acf8f
chrsolr <chr.solr@gmail.com>
2020-12-23 18:39:18 -0600

    Updated

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,11 +1,23 @@
 import React, { useEffect, useState } from 'react'
 import { legendsService, Legends } from '../../services/LegendsService'
-import { Text, View } from 'react-native'
+import { Text, View, StyleSheet } from 'react-native'
 import { createStackNavigator } from '@react-navigation/stack'
 import { enableScreens } from 'react-native-screens'
+import CircleAvatarCard from '../../components/CircleAvatarCard'
+import { ScrollView } from 'react-native-gesture-handler'
+import { dimens } from '../../utils/dimens'
+import { colors } from '../../utils/colors'
+import { Title } from 'react-native-paper'
 enableScreens()
 
 const Stack = createStackNavigator()
+
+const styleSheet = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.background.main,
+  },
+})
 
 export function HomeStackScreen() {
   return (
@@ -18,17 +30,34 @@ export function HomeStackScreen() {
 function HomeScreen({ navigation }) {
   const [legends, setLegends] = useState<Legends[]>([])
   useEffect(() => {
-    const fetchLegends = async () => {
+    ;(async () => {
       const result = await legendsService.getLegends()
       setLegends(result)
-    }
-
-    fetchLegends()
+    })()
   }, [])
 
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      {legends.map((item) => <Text key={item.id}>{item.name}</Text>)}
-    </View>
+    <ScrollView style={styleSheet.container}>
+      <Title>Legends</Title>
+      <ScrollView
+        horizontal={true}
+        showsVerticalScrollIndicator={false}
+        showsHorizontalScrollIndicator={false}
+        decelerationRate="fast"
+        contentContainerStyle={{ padding: dimens.spacing.container }}
+      >
+        {legends.map((item) => {
+          return (
+            <View
+              key={item.id}
+              style={{ marginHorizontal: dimens.spacing.level_1 }}
+            >
+              <CircleAvatarCard uri={item.imageUrl} border={4} size={100} />
+              <Title style={{ textAlign: 'center' }}>{item.name}</Title>
+            </View>
+          )
+        })}
+      </ScrollView>
+    </ScrollView>
   )
 }
---
+++ src/utils/dimens.ts
@@ -0,0 +1,29 @@
+const base = 4
+
+export const dimens = {
+  elevation: {
+    level_1: 4,
+    level_2: 8,
+    level_3: 12,
+    level_4: 16,
+    level_5: 20,
+    level_6: 24,
+    level_7: 28,
+    level_8: 32,
+    level_9: 36,
+    level_10: 40,
+  },
+  spacing: {
+    level_1: 4,
+    level_2: 8,
+    level_3: 12,
+    level_4: 16,
+    level_5: 20,
+    level_6: 24,
+    level_7: 28,
+    level_8: 32,
+    level_9: 36,
+    level_10: 40,
+    container: 16,
+  },
+}
