93188c1c936bb61c3b183d89a4fd9123f0193431
Christian Soler <chr.solr@gmail.com>
2023-01-16 12:26:03 -0600

    Working on Navigation PKGs

--- App.js
+++ App.js
@@ -1,14 +1,76 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useCallback, useState } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import * as SplashScreen from 'expo-splash-screen';
+import { StyleSheet, View } from 'react-native';
+import {
+  useFonts,
+  Exo2_100Thin,
+  Exo2_200ExtraLight,
+  Exo2_300Light,
+  Exo2_400Regular,
+  Exo2_500Medium,
+  Exo2_600SemiBold,
+  Exo2_700Bold,
+  Exo2_800ExtraBold,
+  Exo2_900Black,
+  Exo2_100Thin_Italic,
+  Exo2_200ExtraLight_Italic,
+  Exo2_300Light_Italic,
+  Exo2_400Regular_Italic,
+  Exo2_500Medium_Italic,
+  Exo2_600SemiBold_Italic,
+  Exo2_700Bold_Italic,
+  Exo2_800ExtraBold_Italic,
+  Exo2_900Black_Italic,
+} from '@expo-google-fonts/exo-2'
+import { LegendsScreen } from './src/screens/LegendsScreen';
+
+// Keep the splash screen visible while we fetch resources
+SplashScreen.preventAutoHideAsync();
+const Tab = createBottomTabNavigator()
 
 export default function App() {
+  const [appIsReady, setAppIsReady] = useState(false);
+  let [fontsLoaded] = useFonts({
+    Exo2_100Thin,
+    Exo2_200ExtraLight,
+    Exo2_300Light,
+    Exo2_400Regular,
+    Exo2_500Medium,
+    Exo2_600SemiBold,
+    Exo2_700Bold,
+    Exo2_800ExtraBold,
+    Exo2_900Black,
+    Exo2_100Thin_Italic,
+    Exo2_200ExtraLight_Italic,
+    Exo2_300Light_Italic,
+    Exo2_400Regular_Italic,
+    Exo2_500Medium_Italic,
+    Exo2_600SemiBold_Italic,
+    Exo2_700Bold_Italic,
+    Exo2_800ExtraBold_Italic,
+    Exo2_900Black_Italic,
+  })
+
+  if (!fontsLoaded) {
+    return null;
+  } else {
+    SplashScreen.hideAsync();
+  }
+  
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <NavigationContainer>
+        <Tab.Navigator>
+          <Tab.Screen name="Legends" component={LegendsScreen} options={{ tabBarLabel: 'Legends' }} />
+          <Tab.Screen name="Home" component={LegendsScreen} />
+        </Tab.Navigator>
+      </NavigationContainer>
   );
 }
+
+
 
 const styles = StyleSheet.create({
   container: {
---
+++ src/screens/LegendsScreen.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { SafeAreaView, StatusBar, Text, View } from 'react-native'
+
+export function LegendsScreen({ navigation }) {
+  return (
+    <SafeAreaView
+      style={{
+        flex: 1,
+        backgroundColor: '#FFF'
+      }}
+    >
+      <StatusBar
+        barStyle="dark-content"
+        backgroundColor= '#FFF'
+      />
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Apex Legends buddy!</Text>
+      </View>
+    </SafeAreaView>
+  )
+}
