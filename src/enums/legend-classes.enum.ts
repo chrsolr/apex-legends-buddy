6f8b310e4f20f521958645e9f824510f71a3ca32
chrsolr <christian@innowatts.com>
2023-01-16 10:14:16 -0600

    Fresh Start

--- src/enums/legend-classes.enum.ts
+++
@@ -1,6 +0,0 @@
-export enum LEGEND_CLASSES {
-  OFFENSIVE = 'Offensive Legends',
-  DEFENSIVE = 'Defensive Legends',
-  SUPPORT = 'Support Legends',
-  RECON = 'Recon Legends',
-}
--- src/screens/index.ts
+++
@@ -1,7 +0,0 @@
-import { AccountStackScreen as accountStackScreen } from './Account/Account'
-import { LegendsScreen as legendsScreen } from './Legends/Legends'
-import { HomeScreen as homeScreen } from './Home/Home'
-
-export const AccountScreen = accountStackScreen
-export const LegendsScreen = legendsScreen
-export const HomeScreen = homeScreen
